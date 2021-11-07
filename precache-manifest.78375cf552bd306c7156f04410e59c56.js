8431b0dccde6e71438a548aa8d0155e9d694aa92
itzikyis <itzik.yis@gmail.com>
2021-11-07 08:49:46 +0200

    Updates

---
+++ precache-manifest.78375cf552bd306c7156f04410e59c56.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "070ce673021e586ebf7ed89e8229eafc",
+    "url": "/Netflix/index.html"
+  },
+  {
+    "revision": "850105b3e1daeb3b1d65",
+    "url": "/Netflix/static/css/main.5605d8dd.chunk.css"
+  },
+  {
+    "revision": "bc2c299119743e907a16",
+    "url": "/Netflix/static/js/2.e94b281d.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/Netflix/static/js/2.e94b281d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "850105b3e1daeb3b1d65",
+    "url": "/Netflix/static/js/main.69adcdb0.chunk.js"
+  },
+  {
+    "revision": "2bb5f8bce16eb15942c3",
+    "url": "/Netflix/static/js/runtime-main.537e84a6.js"
+  },
+  {
+    "revision": "dc2411c98b969c2d71b0920ae3e2d9d8",
+    "url": "/Netflix/static/media/bg.dc2411c9.jpg"
+  },
+  {
+    "revision": "35f3a5aec6a2026d1f96de322219136d",
+    "url": "/Netflix/static/media/logo.35f3a5ae.svg"
+  },
+  {
+    "revision": "fab25bc378670766a10e1ec37712945e",
+    "url": "/Netflix/static/media/tab-1-pic.fab25bc3.png"
+  },
+  {
+    "revision": "66c7c1fb5b71b2deaff929bbcd3bacf7",
+    "url": "/Netflix/static/media/tab-macbook.66c7c1fb.png"
+  },
+  {
+    "revision": "dc986d819c053deb6180a34320af2988",
+    "url": "/Netflix/static/media/tab-tablet.dc986d81.png"
+  },
+  {
+    "revision": "9f91203b7c374a590dd727c323d5b6dc",
+    "url": "/Netflix/static/media/tab-tv.9f91203b.png"
+  }
+]);
